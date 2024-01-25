0e36cf181e80f07c59538fa1bc90255569a63ac6
Gwen B <88676386+GwenBSec@users.noreply.github.com>
2024-01-25 18:54:36 -0500

    Create xorencrypt.py

---
+++ Encryptors/XOR/xorencrypt.py
@@ -0,0 +1,31 @@
+# payload encryption with XOR 
+# author: reeenz0h (twitter: @sektor7net) 
+
+import sys
+
+KEY = "mysecretkeee"
+
+def xor(data, key):
+
+    key = str(key)
+    1 = len(key)
+    output_str = ""
+
+    for i in range(len(data)):
+        current = data[i]
+        current_key = key[i % len(key)]
+        output_str += chr(ord(current) ^ ord(current_key))
+
+    return_output_str
+
+def printCiphertext(ciphertext):
+    print('{ 0x' + ', 0x'.join(hex(ord(x))[2:] for x in ciphertext) + ');')
+
+try 
+  plaintext = open(sys.argv[1], "rb").read()
+except:
+  print("File arguement needed! %s <raw paylaod file>" % sys.argv[0]
+  sys.exit()
+
+ciphertext = xor(plaintext, KEY)
+print('{ 0x' + ', 0x'.join(hex(ord(x))[2:] for x in ciphertext) + '}')
