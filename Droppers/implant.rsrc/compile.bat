5ee9d0a7b6ebd34909db805b75ed0f49fcfada71
Gwen B <88676386+GwenBSec@users.noreply.github.com>
2024-01-26 14:13:27 -0500

    Update compile.bat

--- Droppers/implant.rsrc/compile.bat
+++ Droppers/implant.rsrc/compile.bat
@@ -1,3 +1,5 @@
 @ECHO OFF
 
-cl.exe /nologo /0x /MT /W0 /GS- /DNDEBUG /Tcimplant.cpp /link /OUT:implant.exe /SUBSYSTEM:CONSOLE /MACHINE:x64 
+rc resources.rc
+cvtres /MACHINE:x64 /OUT:resources.o resources.res
+cl.exe /nologo /0x /MT /W0 /GS- /DNDEBUG /Tcimplant.cpp /link /OUT:implant.exe /SUBSYSTEM:WINDOWS /MACHINE:x64 resources.o 
